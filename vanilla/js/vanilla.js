980de4cb4de7c23477feb686579779da2bab72c9
Marquis Kurt <software@marquiskurt.net>
2021-02-14 09:55:40 -0500

    💡 [vanilla/js] Update copyright header

--- vanilla/js/vanilla.js
+++ vanilla/js/vanilla.js
@@ -1,3 +1,11 @@
+/* vanilla.js
+ * Utilities for conversions from PyMarkdown to Vanilla
+ * (C) 2021 Marquis Kurt.
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 function getAllExternalLinks() {
     let main = document.getElementById("main-content");
     for (const link of main.getElementsByTagName("a")) {
